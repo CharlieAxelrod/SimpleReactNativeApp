f5a5d1b3893faf00ee3355813633099098fc1798
Charlie Axelrod <earwaxelrod@Charlies-MacBook-Air.local>
2019-02-16 12:50:22 -0800

    Builds out directory structure and component skeletons

---
+++ src/components/Fetcher.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Fetcher extends React.Component {
+  render() {
+    return (
+			<View>
+				<Text>Fetcher</Text>
+			</View>
+		);
+  }
+}
